207aa22b81e6e7158da4c13ffba14134979c92f3
BELBIN BENO RM <85445374+BELBINBENORM@users.noreply.github.com>
2026-03-11 23:36:17 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="vortex-intelligence-suite",
+    version="0.1.0",
+    author="BELBIN BENO RM",
+    author_email="belbin.datascientist@gmail.com",
+    description="An automated feature profiling tool combining statistical diagnostics with LightGBM importance.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/BELBINBENORM/vortex-intelligence-suite",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    ],
+    python_requires='>=3.8',
+    install_requires=[
+        "pandas",
+        "numpy",
+        "matplotlib",
+        "seaborn",
+        "lightgbm",
+        "scipy",
+    ],
+)
